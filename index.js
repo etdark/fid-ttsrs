3483ef726713c24ac5cc7d9bc0a1069f3b114335
et dark <112173901+etdark@users.noreply.github.com>
2022-09-19 01:58:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function learnmore_rd(){
+    alert("heello")
+}
+function onMenuClick() {
+    var navbar = document.getElementById('navigation-bar');
+    var responsive_class_name = 'responsive'
+    
+    navbar.classList.toggle(responsive_class_name)
+
+}
+
+function onMenuClick() {
+    var navbar = document.getElementById('navigation-bar');
+    var responsive_class_name = 'responsive'
+    
+    navbar.classList.toggle(responsive_class_name)
+
+}
