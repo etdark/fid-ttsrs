ca14e0baf4b3431bdd4866bc7e66139d2a285e09
et dark <112173901+etdark@users.noreply.github.com>
2022-09-21 23:25:32 -0700

    Update sr.js

--- sr.js
+++ sr.js
@@ -78,7 +78,7 @@ function done(){
     data.append('file', input)
     fetch("https://tmpfiles.org/api/v1/upload/", {
          method: 'POST',
-         body:data,
+         body:data, 
     
         })
     .then(response => response.json())
@@ -87,7 +87,7 @@ function done(){
           url = Object.values(url_data)[0]
           str_url = JSON. stringify(url)
           final_url = str_url.substr(0,21) + "/dl" + str_url.substr(21) 
-          p_url = "https://amh-rs.herokuapp.com/?url=" + final_url
+          p_url = "https://amhrs.herokuapp.com/?url=" + final_url
           f_url = p_url.replace('"', "").replace('"', "")
           url = f_url
           var xmlHttp = new XMLHttpRequest()
@@ -115,4 +115,106 @@ function upload(){
     document.getElementById("btn-done").style.display = "block"
     document.getElementById("upload").click()
 }
+
+
+
+
+
+
+function done(){
+    document.getElementById("loading").style.display = "block"
+    document.getElementById("record-icon").style.display = "none"
+    document.getElementById("recordinfo").style.display = "none"
+    document.getElementById("upload-text").style.display = "none"
+    document.getElementById("btn-done").style.display = "none"
+    var input = document.getElementById("upload").files[0]
+    const data = new FormData()
+    data.append('file', input)
+    fetch("https://tmpfiles.org/api/v1/upload/", {
+         method: 'POST',
+         body:data, 
+    
+        })
+    .then(response => response.json())
+    .then(data => {
+          url_data = Object.values(data)[1]
+          url = Object.values(url_data)[0]
+          str_url = JSON. stringify(url)
+          final_url = str_url.substr(0,21) + "/dl" + str_url.substr(21) 
+          p_url = "https://amhrs.herokuapp.com/?url=" + final_url
+          f_url = p_url.replace('"', "").replace('"', "")
+          url = f_url
+          var xmlHttp = new XMLHttpRequest()
+          xmlHttp.open( "GET", url )
+          xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == XMLHttpRequest.DONE){
+                transcript = JSON.parse(xmlHttp.responseText)["transcript"]
+                document.getElementById("result-holder").value = transcript
+                document.getElementById("btn-upload").style.display = "block"
+                document.getElementById("loading").style.display = "none"
+                document.getElementById("record-icon").style.display = "block"
+                document.getElementById("recordinfo").style.display = "block"
+                }
+            }
+           xmlHttp.send();
+    })
+    .catch(error => {
+        console.error(error)
+    
+      })
+}
+function upload(){ 
+    document.getElementById("btn-upload").style.display = "none"
+    document.getElementById("upload-text").style.display = "block"
+    document.getElementById("btn-done").style.display = "block"
+    document.getElementById("upload").click()
+}
+
+function copy(){
+    var copy = document.getElementById("copy");
+    copy.style.opacity = "0%"
+    console.log("hi")
+    copyText = document.getElementById("result-holder").value
+    copyText.setSelectionRange(0, 99999)
+    navigator.clipboard.writeText(copyText)
+}
+function cc(){
+    console.log("hi")
+    copyText = document.getElementById("result-holder").value
+
+    navigator.clipboard.writeText(copyText)
+}
+var copy = document.getElementById("copy");
+copy.onmouseover = function() {  copy.style.opacity = "50%" }
+copy.onmouseout = function() {  copy.style.opacity = "100%"}
+var down = document.getElementById("down");
+down.onmouseover = function() {  down.style.opacity = "50%" }
+down.onmouseout = function() {  down.style.opacity = "100%"}
+
+saveBtn = document.getElementById("down")
+saveBtn.addEventListener('click', async function(){
+    let transcript_text = document.getElementById("result-holder");
+    var taBlob = new Blob([transcript_text.value], {type: 'text/plain'});
+    var name = "fid_transcripted"
+    const pickerOptions = {
+      suggestedName: `${name.toLowerCase()}.txt`,
+      types: [
+        {
+          description: 'fid transcript text',
+          accept: {
+            'text/plain': ['.txt'],
+          },
+        },
+      ],
+    };
+   
+    const fileHandle = await window.showSaveFilePicker(pickerOptions);
+   
+    const writableFileStream = await fileHandle.createWritable();
+   
+    await writableFileStream.write(taBlob);
+   
+    await writableFileStream.close();
+  });
+
 
